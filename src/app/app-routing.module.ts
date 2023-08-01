5b83a32ae9ea3df89879fae7bec27d3d8f3fce9e
Pepihristova <45016031+Pepihristova@users.noreply.github.com>
2023-08-01 16:42:25 +0300

    the base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeComponent } from './recipe/recipe.component';
+import { MyRecipeComponent } from './my-recipe/my-recipe.component';
+import { RecreateComponent } from './recreate/recreate.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RecipeComponent},
+  {path: 'home', component: MyRecipeComponent},
+  {path: 'create', component: RecreateComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { MyRecipeComponent } from './my-recipe/my-recipe.component';
+import { RecreateComponent } from './recreate/recreate.component';
+import { AuthModule } from './auth/auth.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    MyRecipeComponent,
+    RecreateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
